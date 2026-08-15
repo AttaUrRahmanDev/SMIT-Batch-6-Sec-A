0199c39efcb5428d6780f697fd4e8de5928b2b46
AttaUrRahmanDev <urrahmana04@gmail.com>
2026-08-15 10:57:29 +0500

    Intro To JS

---
+++ JavaScript/class_1/intro/index.js
@@ -0,0 +1,14 @@
+console.log("Welcome to js")
+// document.write("hello world!    ")
+
+
+// alert("Hello World")
+// confirm("Do You Want To Delete xyz Product.")
+
+// var userInput = prompt("Please Enter Student Marks",)
+// alert("Grade A+")
+var userAge=prompt("Please Enter Your Age!",18  )
+
+
+
+alert("You Are Eligible")
