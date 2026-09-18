3fb11a1a2e0de10cf3b435380ec204e1d1f6c430
AttaUrRahmanDev <urrahmana04@gmail.com>
2026-09-18 16:06:02 +0500

    Add js-assignment-1

---
+++ JavaScript/js-assignment-1/script.js
@@ -0,0 +1,17 @@
+alert("Hello World!"); // Display Hello World In Alert Box.
+console.log("Hello World!"); //Write Hello World In Console.
+
+var name = "Atta Ur Rahman"; // Variable Declaration
+name = "Atta Ur Rahman2"; // Variable Initialization
+let age = 21; // Variable Declaration and Initialization
+const education = "Undergraduate"; // Variable Declaration and Initialization
+console.log("Name:" + name); // Write Name In Console
+console.log("Age:" + age); // Write Age In Console
+console.log("Education:" + education); // Write Education In Console
+document.write("Name:" + name); // Write Name In Document
+
+age = 22; //Can Be Reassigned And Updated Because It Is Declared With Let
+console.log("Updated Age:" + age); // Write Updated Age In Console
+education = "Graduate"; // This will throw an error because education is a constant and cannot be reassigned.
+console.log(education); // This line will not be executed due to the error above.
+
