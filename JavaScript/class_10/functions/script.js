9b2770854cb99cbbdddc1c3ca16ec3c5abbf2d99
AttaUrRahmanDev <urrahmana04@gmail.com>
2026-09-20 10:22:03 +0500

    loops: for of,Tasks,Functions

---
+++ JavaScript/class_10/functions/script.js
@@ -0,0 +1,23 @@
+/*
+Syntax:-
+function functionName(parameters) {
+    // code to be executed[body]
+}
+
+functionName(arguments);
+*/
+
+function welcome(name) {
+  console.log(`Welcome to ${name}`);
+}
+
+// welcome();
+// welcome();
+// welcome();
+// welcome();
+// welcome();
+// welcome();
+// welcome();
+
+let studentName = prompt("Enter Student Name: ");
+welcome(studentName); //Argument
---
+++ JavaScript/class_10/loops/script.js
@@ -0,0 +1,88 @@
+// let students=["test1","test2","test9","test4"]
+// let inputStudentName=prompt("Enter Student Name:")
+
+// for(let student of students){
+//     if(inputStudentName ===  student){
+//         // console.log(student);
+//         // break;
+//         continue;
+
+
+
+//     }
+    
+//         console.log(student);
+    
+    
+// }
+
+
+// Task-1
+// for(num=0;num<=20;num++){
+//     console.log(num)
+// }
+
+// Task-2
+// for(a=0;a<=50;a++){
+
+//     if (a%2==0){
+//         console.log(a)
+//     }
+// }
+
+
+
+
+
+// // Task-3
+// for(num=10;num>=1;num--){
+//     console.log(num)
+// }
+
+// console.log("Happy New Year")
+
+// Task-4
+num=Number(prompt("Enter A Number"))
+for(i=1;i<=10;i++){
+    console.log(num*i)
+}
+
+// Task 5
+
+
+
+
+
+// //Task-7
+// num=Number(prompt("Enter A Number: "))
+// while (true){
+//     if (num===num){
+//         console.log("Success")
+//         break;
+        
+//     }
+//     else{
+//         console.log("Failed")
+
+//     }
+// }
+
+
+
+
+
+// let marks=[85, 92, 78, 96]
+// let total=0;
+// for(let mark of marks){
+//     total=total+mark;
+
+//     console.log(mark);
+// }
+// console.log("Total Marks: "+total)
+ 
+
+function greet(name){
+    name=prompt("Enter Your Name:")
+    console.log("Hello, "+name+"!")
+}   
+greet()
